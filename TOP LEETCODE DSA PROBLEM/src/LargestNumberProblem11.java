d0851bd5c0d28f920895b4c09bec1105ce2ddf64
alok99maurya <alok99maurya@gmail.com>
2025-04-06 08:59:54 +0530

    initial-commit

---
+++ TOP LEETCODE DSA PROBLEM/src/LargestNumberProblem11.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class LargestNumberProblem11 {
+
+	public static void main(String[] args) {
+		int[] arr = {2,1,3,2,4};
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
+
+	}
+
+}
